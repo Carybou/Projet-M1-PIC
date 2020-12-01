55168a370756a2bf7bdb35e1e58976055374ed5c
Niaouh <https://cbegue@redmine.laas.fr>
2020-12-01 17:14:29 +0100

    clone

--- PIC_simul/pic.jl
+++ PIC_simul/pic.jl
@@ -4,63 +4,30 @@ using ProgressMeter
 
 include("pic1d.jl")
 
-# +
-xmin = 0
-xmax = 1
-nx = 64
-e0 = 1
-c = 1
-md, nd = 2, 2  # number of modes along each direction
-dt = 0.001
-nstep = 1 ÷ dt
-mesh = Mesh1D(xmin, xmax, nx)
-#maxwell = MaxwellSolver(mesh, c, e0)
-omega = c * sqrt((md * pi / dimx)^2)
-ex = zeros(nx + 1)
-bz = zeros(nx + 1)
+kernel_d(pi, 1)
 
-# -
-
-surface(bz, aspect_ratio = :equal, zlims = (-1, 1))
-
-# Ex and Ey are set at t = 0.0
-# Bz is set at  t = -dt/2
+(x0, v0, wei) = samples(1000, 0.5, 0.5)
+plot(v0)
+plot(x0)
 
 # +
 function run(nstep)
-    dimx, dimy = 1, 1
-    nx, ny = 64, 64
-    e0 = 1
-    c = 1
-    md, nd = 2, 2  # number of modes along each direction
+    xmin =-pi
+    xmax = pi
+    vmin = -1
+    vmax = 1
+    k = 1
+    nx = 64
     dt = 0.001
-    mesh = Mesh(dimx, nx, dimy, ny)
-    maxwell = MaxwellSolver(mesh, c, e0)
-    omega = c * sqrt((md * pi / dimx)^2 + (nd * pi / dimy)^2)
-    ex = zeros(nx + 1, ny + 1)
-    ey = zeros(nx + 1, ny + 1)
-    bz = zeros(nx + 1, ny + 1)
-
-    x = 0.5 .* (mesh.x[1:end-1] .+ mesh.x[2:end])
-    y = transpose(0.5 .* (mesh.y[1:end-1] .+ mesh.y[2:end]))
-
-    maxwell.bz .= -cos.(md * pi * x) .* cos.(nd * pi * y) .* cos(omega * (-0.5 * dt))
-
-
+    mesh = Mesh1D(xmin, xmax, nx)
+    (x0, y0, wei) = samples(nx, 0.5, 0.5)
+    histogram(x0)
+    p = Particles(x0, y0, wei)
     @gif for istep = 1:nstep # Loop over time
-
-        faraday!(maxwell, bz, dt)
-
-        ampere_maxwell!(maxwell, ex, ey, dt)
-
-        #p = plot(layout=(1,2))
-        #surface!( p[1,1], ex, aspect_ratio=:equal, zlims=(-1,1))
-        #surface!( p[1,2], ey, aspect_ratio=:equal, zlims=(-1,1))
-        surface(bz, aspect_ratio = :equal, zlims = (-1, 1))
-
+        update_velocities!(p, k, dt)
+        update_positions!(p, mesh, dt)
+        histogram(p.x)
     end every (nstep ÷ 100)
-
-
 end
 
 # -
--- PIC_simul/pic1d.jl
+++ PIC_simul/pic1d.jl
@@ -1,3 +1,5 @@
+using Sobol
+
 export Mesh1D
 
 """
@@ -11,64 +13,136 @@ struct Mesh1D
    dx #distance between 2 samples
 
    function Mesh1D( xmin, xmax, nx)
-
        dx = (xmax - xmin) / nx
-
        new(xmin, xmax, nx, dx)
-
    end
 
 end
 
-export Particles1D
+export Particles
 
 """
 Describe meta particules, represented by a Dirac disbtibution in (x, v), with a weight (~a high) of wei
 """
-struct Particles1D
+struct Particles
 
-    wei #list of the weights of the particules
     x #list of the positions
     v #list of the velocities
+    wei #list of the weights of the particules
 
+    function Particles(x0, v0, wei)
+        new(x0, v0, wei)
+    end
 end
 
 """
 Define the kernel of the model, is n = 1 kernel = cos (HMF model)
 """
 function kernel(x, n)
-    sum(1/k^2 * exp(im * k * x) for k in -1*n:n if k != 0)
+    return(sum(1/k^2 * exp(im * k * x) for k in -1*n:n if k != 0))
 end
 
 """
 Define the derivate kernel of the model, is n = 1 kernel = cos (HMF model)
 """
 function kernel_d(x, n)
-    sum(im/k * exp(im * k * x) for k in -1*n:n if k != 0)
+    return(sum(im/k * exp(im * k * x) for k in -1*n:n if k != 0))
 end
 
 export update_positions!
 
 """
-update particle position xp
+update particle position xp (phi_T)
 """
 function update_positions!(p, mesh, dt)
 
     xmin = mesh.xmin
     xmax = mesh.xmax
 
-    p.xp .+= p.vp .* dt
-    p.xp .= xmin .+ mod.( p.xp .- xmin , xmax - xmin)
+    p.x .+= p.v .* dt
+    #faire un modulo sans int
 
 end
 
 export update_velocities!
 
 """
-update particle velocities vp
+update particle velocities vp (phi_v)
 """
-function update_velocities!(p, e, coeffs, dt)
+function update_velocities!(p, k, dt)
 
-    p.vp .+= coeffs * e .* p.qm .* dt
+    to_add=[]
+    for position in p.x
+        to_push = - dt * sum(p.wei .* kernel_d.(position .- p.x, k))
+        push!(to_add, to_push)
+    end
+    p.v .+= to_add #reverse ?
 
+end
+
+"""
+generated random samples
+"""
+function random_generation(nsamples, mesh, vmin, vmax, f0)
+    x0 = []
+    v0 = []
+    wei = []
+    s = SobolSeq(1)
+
+    for i=1:nsamples
+        x = Sobol.next!(s)
+        x = pop!(x)
+        x = (1-x)* mesh.xmin + x*mesh.xmax
+        v = Sobol.next!(s)
+        v = pop!(v)
+        v = (1-v) * vmin + v*vmax
+        push!(x0, f0(x))
+        push!(v0, v)
+        push!(wei, 1/nsamples)
+    end
+    return (x0, v0, wei)
+end
+
+"""
+```math
+f_0(x,v,t) = \\frac{n_0}{2π v_{th}^2} ( 1 + \\alpha cos(k_x x))
+ exp( - \\frac{v^2}{2 v_{th}^2})
+```
+The newton function solves the equation ``P(x)-r=0`` with Newton’s method
+```math
+x^{n+1} = x^n – (P(x)-(2\\pi r / k)/f(x)
+```
+with
+```math
+P(x) = \\int_0^x (1 + \\alpha cos(k_x y)) dy = x + \\frac{\\alpha}{k_x} sin(k_x x)
+```
+"""
+
+function samples(nsamples, alpha, kx)
+
+    function newton(r)
+        x1, x2 = 0.0, 1.0
+        r *= 2π / kx
+        while (abs(x2-x1) > 1e-12)
+            p = x1 + alpha * sin( kx * x1) / kx
+            f = 1 + alpha * cos( kx * x1)
+            x1, x2 = x2, x1 - (p - r) / f
+        end
+        return x2
+    end
+
+    x0 = []
+    v0 = []
+    wei = []
+    s = SobolSeq(1)
+
+    for i=1:nsamples
+        v = sqrt(-2 * log( (i-0.5)/nsamples))
+        x = pop!(Sobol.next!(s))
+        x = newton(x)
+        push!(x0, x)
+        push!(v0, v)
+        push!(wei, 2*pi/kx/nsamples)
+    end
+    return x0, v0, wei
 end
