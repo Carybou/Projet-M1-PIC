f9bb8a538e7132b2fd1811cb1056c4d0aa1f4e0a
Niaouh <https://cbegue@redmine.laas.fr>
2021-02-11 19:51:59 +0100

    calcul energy pic et hamil ok, distribution initiale ok

--- PIC_simul/pic.jl
+++ PIC_simul/pic.jl
@@ -3,25 +3,25 @@ using ProgressMeter
 using SparseArrays
 using LinearAlgebra
 
-
 include("pic1d.jl")
-
-"""xmin =0
+"""
+xmin =0
 xmax = 4pi
 k = 1
 nx = 30
 
 dt = 0.001
 m = Mesh1D(xmin, xmax, nx)
-(x0, y0, wei) = samples(1000, 0.5, 0.5)
+(x0, y0, wei) = samples_2(1000, xmin, xmax)
 p = Particules(x0, y0, wei, 1000)
 r, rt = compute_rho(p, m)
 plot(r)
 plot(y0)
-plot(size  = (500,500),
-        legend=false, widen  = false)
+plot(size  = (500,500), widen  = false)
 
-histogram!(x0, bins = 50, normalize = true)
+#fit(Histogram, (x0, y0), bins = 50)
+histogram2d (x0[1:1:end], y0[1:1:end], bins = 100)
+plot(x0, y0)
 
 nx = 6
 matrix_poisson = spdiagm(-1 => ones(Float64,nx-1),
@@ -33,13 +33,15 @@ det(matrix_poisson)"""
 function run(nstep)
     xmin = 0
     xmax = 4pi
-    k = 5 #paramètre de troncature du noyau 
+    k = 1 #paramètre de troncature du noyau
     np = 10000 #nb de particules
-    nx = 300   #nb de mailles pour le calcul de l energie
+    nx = 100   #nb de mailles pour le calcul de l energie
 
     dt = 0.1
+    alpha = 0.5
+    kx = 2pi /(xmax -xmin)
     mesh = Mesh1D(xmin, xmax, nx)
-    (x0, y0, wei) = samples(np, 0.5, 0.5)
+    (x0, y0, wei) = samples_2(np, alpha, kx)
     p = Particules(x0, y0, wei, np)
 
 
@@ -47,34 +49,51 @@ function run(nstep)
                                 0 => -2*ones(Float64,nx+1),
                                 1 => ones(Float64,nx-1))
 
-    E = []
     rho_i, rho_ti = compute_rho(p, mesh)
     phi_i = compute_phi(rho_i, rho_ti, mesh, matrix_poisson)
     e = compute_E(phi_i, mesh)
     potential = []
+    energy_pic = []
+    energy_hamil_elec = []
+    energy_hamil_cine = []
+    energy_tot = []
     anime = @animate for istep = 1:nstep # Loop over time
-        push!(potential, update_velocities_S_C!(p, k, dt))
+        plot(size  = (500,500), ylim = (0,.5),
+            widen = false, st=:surface, camera=(-30,30))
+        histogram2d(real(p.x)[1:1:end], real(p.v)[1:1:end], bins = nx, normalize = true)
+
+        energy_cine = 1/2 * sum(p.wei .* (p.v .* p.v))
+
+        phi_t = update_velocities!(p, k, dt, kx)
+        push!(energy_pic, compute_int_E(p, mesh, matrix_poisson))
         update_positions!(p, mesh, dt)
-        plot(size  = (500,500), xlim = (xmin, xmax), ylim = (0,.5),
-                legend=false, widen = false)
-        histogram!(real(p.x), bins = nx, normalize = true)
-        push!(E, compute_int_E(p, mesh, matrix_poisson))
+        energy_elec = sum(p.wei .* phi_t)
+
+        push!(potential, phi_t)
+        push!(energy_hamil_elec, energy_elec)
+        push!(energy_hamil_cine, energy_cine)
+        push!(energy_tot, energy_elec + energy_cine)
     end
     #plot(potential)
-    """pot = pop!(potential)
-    plot(size  = (500,500),
-            legend=false, widen = false)
-    plot!(real(p.x), real(pot), seriestype = :scatter) #sin x /0.4 pi + dephasage, phi
-    rho, rho_t = compute_rho(p, mesh)
-    phi = compute_phi(rho, mesh, matrix_poisson)
-    plot(phi_i)
-    plot!(phi)
-    plot(E, yaxis=:log)"""
-    plot(p.x, pop!(potential), seriestype = :scatter)
+    """rho, rho_t = compute_rho(p, mesh)
+    plot(real(p.x), real(pot), seriestype = :scatter)
+    phi = compute_phi(rho, rho_t, mesh, matrix_poisson)
+    plot(phi)
+    plot!(energy_hamil)
+    """
+    plot(real(rho_i))
+    plot!(phi_i)
     #plot!(phi_i)
-    #gif(anime)
+    #plot(p.x, pop!(potential), seriestypes = :scatter)
+    plot(energy_pic)
+    plot!(energy_hamil_elec)
+    plot!(energy_tot)
+    gif(anime)
 end
 
 # -
 
-run(100)
+run(300)
+# faire distribution / check 3 méthodes de calcul de phi (PIC (ok), hamiltonien (ok), exact et th)
+# coeff dans calcul hamiltonien avec TFD N(x) = \sum 1/(2pi*(k*kx)^2) exp(i*kx*k*x) x \in [0, 2pi/kx],
+# qui est C ? (1/2pi ?)
--- PIC_simul/pic1d.jl
+++ PIC_simul/pic1d.jl
@@ -1,4 +1,6 @@
 using Sobol
+using Random
+using Distributions
 
 """
 Describe the universe of the plasma : one dimension, between 2 bounds xmin and xmax
@@ -47,12 +49,14 @@ function kernel_d(x, n)
 end
 """ attention ffttd donc facteur dans la somme  a voir """
 
-function compute_S_C(p, n)
+""" S[k] = \\sum_l=1^n {\\beta_l * sin(l x_l)} et C[k] = \\sum_l=1^n {\\beta_l * cos(l x_l)}
+utile pour le calcul de la vitesse et du potentiel electrique phi"""
+function compute_S_C(p, n, kx)
     S = []
     C = []
     for k in 1:n
-        push!(S, sum(p.wei .* sin.(k .* p.x)))
-        push!(C, sum(p.wei .* cos.(k .* p.x)))
+        push!(S, sum(p.wei .* sin.((k * kx) .* p.x)))
+        push!(C, sum(p.wei .* cos.((k * kx) .* p.x)))
     end
     return S, C
 end
@@ -74,40 +78,27 @@ function mod_float(p, xmin, xmax)
 end
 
 function update_positions!(p, mesh, dt)
-
     xmin = mesh.xmin
     xmax = mesh.xmax
 
     p.x .+= p.v .* dt
     p.x .= mod_float(p.x, xmin, xmax)
-    #faire un modulo sans int
-
 end
 
 """
 update particle velocities vp (phi_v)
 """
-function update_velocities!(p, k, dt)
-
-    to_add=[]
-    phi = []
-    for position in p.x
-        to_push = - sum(p.wei .* kernel_d.(position .- p.x, k))
-        push!(to_add, dt * to_push)
-        push!(phi, to_push)
-    end
-    p.v .+= to_add
-    return phi
-end
-
-function update_velocities_S_C!(p, n, dt)
-    S, C = compute_S_C(p, n)
+function update_velocities!(p, n, dt, kx)
+    S, C = compute_S_C(p, n, kx)
     to_add = []
     phi = []
     for position in p.x
-        to_push =  sum([2/k * (cos(k*position) * S[k] - sin(k*position) * C[k]) for k in 1:n])
-        push!(to_add, - dt * to_push)
-        push!(phi, to_push)
+        to_push = - dt * sum([2/(2pi*k*kx) * (cos(k*kx*position) * S[k] -
+                                    sin(k*kx*position) * C[k]) for k in 1:n])
+        to_push_e = sum([1/(2pi*(k*kx)^2) * (cos(k*kx*position) * C[k] -
+                                        sin(k*kx*position) * S[k]) for k in 1:n])
+        push!(to_add, to_push)
+        push!(phi, to_push_e)
     end
     p.v .+= to_add
     return phi
@@ -116,7 +107,7 @@ end
 """
 ```math
 f_0(x,v,t) = \\frac{n_0}{2π v_{th}^2} ( 1 + \\alpha cos(k_x x))
- exp( - \\frac{v^2}{    2 v_{th}^2})
+ exp( - \\frac{v^2}{2})
 ```
 The newton function solves the equation ``P(x)-r=0`` with Newton’s method
 ```math
@@ -131,7 +122,6 @@ voir package dictrbution.jl
 """
 
 function samples(nsamples, alpha, kx)
-
     function newton(r)
         x1, x2 = 0.0, 1.0
         r *= 2π / kx
@@ -142,20 +132,43 @@ function samples(nsamples, alpha, kx)
         end
         return x2
     end
-
     x0 = []
     v0 = []
     wei = []
     s = SobolSeq(1)
-
     for i=1:nsamples
-        v = sqrt(-2 * log( (i-0.5)/nsamples))
+        #v = sqrt(-2 * log( (i-0.5)/nsamples)) * rand([-1,1])
+        v = randn()
         x = pop!(Sobol.next!(s))
         x = newton(x)
         push!(x0, x)
         push!(v0, v)
         push!(wei, 2*pi/kx/nsamples)
     end
+    return x0, v0, wei
+end
+
+function samples_2(nsamples, alpha, kx)
+    function newton(r)
+        x1, x2 = 0.0, 1.0
+        r *= 2π / kx
+        while (abs(x2-x1) > 1e-12)
+            p = x1 + alpha * sin( kx * x1) / kx #primitive 1 + alpha cos(kx x)
+            f = 1 + alpha * cos( kx * x1)
+            x1, x2 = x2, x1 - (p - r) / f
+        end
+        return x2
+    end
+    x0 = []
+    s = SobolSeq(1)
+    for i=1:nsamples
+        x = pop!(Sobol.next!(s))
+        x = newton(x)
+        push!(x0, x)
+    end
+    distr_v = Normal()
+    v0 = rand(distr_v, nsamples)
+    wei = [2*pi/kx/nsamples for i=1:nsamples]
     return x0, v0, wei
 end
 
@@ -171,22 +184,15 @@ function compute_rho( p, m )
    rho = zeros(nx + 1)
 
    for ipart=1:p.nbpart
-
       xp = p.x[ipart]
-
-  i = Int(floor(Real(xp) / dx) + 1) #number of the cell with the particule
-
+      i = Int(floor(Real(xp) / dx) + 1) #number of the cell with the particule
       dum = p.wei[ipart] / dx
-
       mx_i = (i-1) * dx
       mx_j = i * dx
-
       a1 = (mx_j-xp) * dum
       a2 = (xp-mx_i) * dum
-
       rho[i]     +=  a1
       rho[i+1]   +=  a2
-
    end
 
    rho[nx+1] += rho[1]
@@ -195,7 +201,6 @@ function compute_rho( p, m )
    rho ./= dx
 
    rho_total  = sum(rho[1:nx]) * dx / (xmax - xmin)
-
    return rho, rho_total
 end
 
@@ -226,7 +231,7 @@ end
 function compute_int_E(p, mesh, matrix_poisson)
     dx = mesh.dx
     rho, rho_t = compute_rho(p, mesh)
-    phi = compute_phi(rho, mesh, matrix_poisson)
+    phi = compute_phi(rho, rho_t, mesh, matrix_poisson)
     E = compute_E(phi, mesh)
     return sum(E .^2) * dx
 end
